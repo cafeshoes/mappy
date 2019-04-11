f1e81b67dc81911358a41d27510b5662549fbd76
Shubhesh Dwivedi <frostcover@gmail.com>
2019-04-12 05:04:03 +0530

    fix

--- index.js
+++ index.js
@@ -162,6 +162,6 @@ function isLoggedIn(req, res, next){
 	res.redirect("/login");
 }
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
 	console.log("Server started..!!!")
 });
